189a8231bcdf9339c87720e43a2e51f40cf5b8b2
Meghana <msrikala2k7@gmail.com>
2026-06-25 12:12:31 +0530

    Add for loop examples for numbers and countdown

--- day04_for_loops.py
+++ day04_for_loops.py
@@ -2,16 +2,19 @@ NUMBERS 1-10:
 for i in range(1,11):
     print(i)
 
+---------------------------------------
 
 WITH NAME:
 for i in range(1,11):
     print("Meghana",i)
 
+---------------------------------------
 
 7-TABLE:
 for i in range(1,11):
     print("7x",i,"=",7*i)
 
+---------------------------------------
 
 COUNTDOWN:
 for i in range(10,0,-1):
